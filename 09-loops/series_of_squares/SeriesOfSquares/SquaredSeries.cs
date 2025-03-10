49d7ff872dc19cc89795f7a0728704134b113a5d
Michiel Decoster <decoster.michiel@gmail.com>
2025-03-10 19:21:02 +0100

    Finished the exercise

--- 09-loops/series_of_squares/SeriesOfSquares/SquaredSeries.cs
+++ 09-loops/series_of_squares/SeriesOfSquares/SquaredSeries.cs
@@ -9,13 +9,18 @@ namespace SeriesOfSquares
         public static int SeriesSum(int n)
         {
             // TODO - Determine the sum of a squared series using a simple loop
-            return -1;
+            int sum = 0;
+            for(int i = 0; i <= n;) {
+                sum = sum + (i * i);
+                i++;
+            }
+            return sum;
         }
 
         public static int CalculateSumSeries(int n)
         {
             // TODO - Determine the sum using the formula for a squared series
-            return -1;
+            return (n * (n + 1) * (2 * n + 1)) / 6; ;
         }
     }
 }
