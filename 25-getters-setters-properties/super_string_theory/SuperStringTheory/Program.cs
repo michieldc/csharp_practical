d17a1e27182985f3a016ab3ec58ce4aafd0fb5bb
Michiel Decoster <decoster.michiel@gmail.com>
2025-05-06 21:12:46 +0200

    Finished the Super String Theory exercise

--- 25-getters-setters-properties/super_string_theory/SuperStringTheory/Program.cs
+++ 25-getters-setters-properties/super_string_theory/SuperStringTheory/Program.cs
@@ -12,38 +12,54 @@ namespace SuperStringTheory
             Console.WriteLine("Lets append the word \" World\" to \"Hello\"");
             SuperString hello = new SuperString();
 
-            hello.SetText("Hello");
-            Console.WriteLine(" > Before: " + hello.GetText());
+            hello.Text = ("Hello");
+            Console.WriteLine(" > Before: " + hello.Text);
 
             hello.Append(" World");
-            Console.WriteLine(" > After: " + hello.GetText());
+            Console.WriteLine(" > After: " + hello.Text);
 
             // IsValidNumber
             Console.WriteLine("\nLet's check some numbers ...");
             SuperString number = new SuperString();
 
-            number.SetText("1234");
-            Console.WriteLine($"Is {number.GetText()} a valid number? " + number.IsValidNumber());
+            number.Text = ("1234");
+            Console.WriteLine($"Is {number.Text} a valid number? " + number.IsValidNumber());
 
-            number.SetText("SDFNBDSF");
-            Console.WriteLine($"Is {number.GetText()} a valid number? " + number.IsValidNumber());
+            number.Text = ("SDFNBDSF");
+            Console.WriteLine($"Is {number.Text} a valid number? " + number.IsValidNumber());
 
-            number.SetText("#88");
-            Console.WriteLine($"Is {number.GetText()} a valid number? " + number.IsValidNumber());
+            number.Text = ("#88");
+            Console.WriteLine($"Is {number.Text} a valid number? " + number.IsValidNumber());
 
-            // Trim
+            //Trim
             Console.WriteLine("\nOK, now for some trimming ...");
             SuperString text = new SuperString();
 
-            text.SetText("  Hello There!");
-            Console.Write($"If we trim at the beginning \"{text.GetText()}\"");
+            text.Text = ("  Hello There!");
+            Console.Write($"If we trim at the beginning \"{text.Text}\"");
             text.TrimStart();
-            Console.WriteLine($", then we get \"{text.GetText()}\"");
+            Console.WriteLine($", then we get \"{text.Text}\"");
 
-            text.SetText("There is no place like home!    ");
-            Console.Write($"If we trim at the beginning \"{text.GetText()}\"");
+            text.Text = ("There is no place like home!    ");
+            Console.Write($"If we trim at the beginning \"{text.Text}\"");
             text.TrimStart();
-            Console.WriteLine($", then we get \"{text.GetText()}\"");
+            Console.WriteLine($", then we get \"{text.Text}\"");
+
+            text.Text = ("Testtrim trailing text.  ");
+            Console.WriteLine($"So we want to trim \"{text.Text}\"");
+            text.TrimEnd();
+            Console.WriteLine($", then we get \"{text.Text}\"");
+
+            //Prepend
+
+            text.Text = ("Stringtest!");
+            Console.Write($"If we don't prepend the text \"{text.Text}\"");
+            text.Prepend("WAT?");
+            Console.WriteLine($", then we get \"{text.Text}\"");
+
+            text.Text = ("  Hello  ");
+            text.Trim();
+            Console.WriteLine(text.Text);
         }
     }
 }
--- 25-getters-setters-properties/super_string_theory/SuperStringTheory/SuperString.cs
+++ 25-getters-setters-properties/super_string_theory/SuperStringTheory/SuperString.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using System.Linq;
 using System.Text;
 
 namespace SuperStringTheory
@@ -8,42 +11,117 @@ namespace SuperStringTheory
     {
         // Text getter method
         // TODO - Replace with property
-        public string GetText()
-        {
-            return text;
-        }
-
-        // Text setter method
-        // TODO - Replace with property
-        public void SetText(string text)
-        {
-            this.text = text;
+        public string Text { 
+            get { return text; } 
+            set {
+                if (value == null) {
+                    text = "";
+                }
+                else { text = value; 
+                }
+            }
+           
         }
 
 
         public void Append(string text)
         {
-            // TODO - Append the local argument text behind the attribute text
+            this.text += text;
         }
 
 
         // TODO - Add a Prepend() method
+        public void Prepend(string text)
+        {
+            int fullLength = text.Length + this.text.Length;
+            Console.WriteLine(fullLength);
+            char[] fullText = new char[fullLength];
+            for (int i = 0; i < text.Length; i++)
+            {
+                fullText[i] = text[i];
+            }
+
+            Console.WriteLine(fullText);
+            int index = 0;
+            for(int j = text.Length; j < fullLength; j++)
+            {
+                fullText[j] = this.text[index];
+                index++;
+            }
+
+            this.text = new string(fullText);
+
+        }
 
         public bool IsValidNumber()
         {
-            // TODO - Check if text is a valid number.
-
-            return false;
+            if(text == "")
+            {
+               return false;
+            }
+            foreach (char c in text)
+            {
+                if (c < '0' || c > '9')
+                {
+                    return false;
+                }
+            }
+                   return true;
         }
 
         public void TrimStart()
         {
             // TODO - Remove any spaces at the beginning of text.
+            string trimmedText = "";
+            bool foundWhitespaceCharacter = false;
+            for (int i = 0; i < text.Length;i++ ) {
+                if (text[i] == ' ')
+                {
+                    // do nothing
+                }
+                else
+                {
+                    foundWhitespaceCharacter = true;
+                }
+
+                if (foundWhitespaceCharacter)
+                {
+                    trimmedText += text[i];
+                }
+            }
+            this.text = trimmedText;
         }
 
         // TODO - Add a TrimEnd() method
+        public void TrimEnd()
+        {
+            int index = text.Length - 1; // we creëren een variabel om te loopen door de string
+            while (index >= 0 && text[index] == ' ') //zolang de variabel >= 0 en een spatie is decrementeren we
+            {
+                index--;
+            }
+            char[] stringArray = new char[index + 1];
+            for (int i = 0; i <= index; i++) {
+                stringArray[i] = text[i];
+            }
+            this.text = new string(stringArray);
+        }
 
         // TODO - Add a Trim() method
+
+        public void Trim()
+        {
+            if (this.text == null)
+            {
+                //do nothing
+            }
+            else 
+            {
+                text = text.TrimStart();
+                text = text.TrimEnd();
+            }
+                
+        }
 
         // Attributes
         private string text = "";
