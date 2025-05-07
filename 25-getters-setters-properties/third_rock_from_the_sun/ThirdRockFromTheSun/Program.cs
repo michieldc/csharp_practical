d8617eb3f4e6e0fd0569dcda35e20acea1efb54b
Michiel Decoster <decoster.michiel@gmail.com>
2025-05-07 19:21:36 +0200

    Finished the exercise

--- 25-getters-setters-properties/third_rock_from_the_sun/ThirdRockFromTheSun/Program.cs
+++ 25-getters-setters-properties/third_rock_from_the_sun/ThirdRockFromTheSun/Program.cs
@@ -7,6 +7,14 @@ namespace ThirdRockFromTheSun
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Sphere sphere = new Sphere();
+
+            sphere.Radius = 6371;
+            Console.WriteLine($"The Earth has a radius of is {sphere.Radius} km");
+            Console.WriteLine($"The means its diameter is about {sphere.Diameter} km");
+            Console.WriteLine($"It has a surface of about {Math.Round(sphere.Surface(),0)} squared kilometers");
+            Console.WriteLine($"It's volume approximates {Math.Round(sphere.Volume(),0)} cubic kilometers");
         }
     }
 }
---
+++ 25-getters-setters-properties/third_rock_from_the_sun/ThirdRockFromTheSun/Sphere.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdRockFromTheSun
+{
+    public class Sphere
+    {
+        public double Radius 
+        { 
+            get
+            {
+                return this.radius;
+            }
+            set 
+            { 
+                if (value <= 0)
+                {
+                    this.radius = Math.Abs(value);
+                }
+                else
+                {
+                    this.radius = value;
+                }
+            } 
+        }
+
+        public double Diameter 
+        {
+            get
+            {
+                return this.radius * 2;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    this.radius = Math.Abs(value) / 2;
+                }
+                else
+                {
+                    this.radius = value / 2;
+                }
+            }
+        }
+
+        public double Volume()
+        {
+            return (4.0 / 3.0 * Math.PI * Math.Pow(this.radius, 3)); //Merk op de 4.0 en 3.0 - voor double division te doen en niet int division
+        }
+
+        public double Circumference()
+        {
+            return (2 * Math.PI * this.radius);
+        }
+
+        public double Surface()
+        {
+            return (4 * Math.PI ) * Math.Pow(this.radius, 2);
+        }
+
+        private double radius;
+    }
+}
